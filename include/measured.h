8d241a738c5c371770afdfc5ec473f8b142633f9
Kasper Kyllonen <nkapu@iki.fi>
2021-04-16 10:17:04 +0300

    Added files measured.c and measured.h for measurements

---
+++ include/measured.h
@@ -0,0 +1,11 @@
+/**
+* Copyright 2021 Nokia
+* Licensed under the GNU General Public License v2.0 only
+* SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#ifndef H_MEASURED_
+
+#define H_MEASURED_
+
+void measured_test(void);
---
+++ src/measured.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2021 Nokia
+ * Licensed under the GNU General Public License v2.0 only
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+void measured_test(void) {
+    int i = 0
+
+}
